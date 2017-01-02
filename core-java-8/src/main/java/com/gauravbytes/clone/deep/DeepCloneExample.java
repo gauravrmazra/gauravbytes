068699d9993075b1d60f09e54884ff31f1f30c73
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-01-02 20:50:57 +0530

    refactoring

--- core-java-8/src/main/java/com/gauravbytes/clone/deep/DeepCloneExample.java
+++ core-java-8/src/main/java/com/gauravbytes/clone/deep/DeepCloneExample.java
@@ -2,7 +2,7 @@ package com.gauravbytes.clone.deep;
 
 public class DeepCloneExample {
 	public static void main(String[] args) throws CloneNotSupportedException {
-		Employee employee = new Employee(1, "Eric", new Address("Sector 37C", "Chandigarh", "India"));
+		Employee employee = new Employee(1, "Gaurav", new Address("Sector 37C", "Chandigarh", "India"));
 		Employee deepClone = (Employee) employee.clone();
 		employee.getAddress().setCity("Hoshiarpur");
 		// should return false
