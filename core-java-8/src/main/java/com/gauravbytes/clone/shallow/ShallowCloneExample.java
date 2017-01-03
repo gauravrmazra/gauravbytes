71bbf34968b08427c13eb45bdba455f5c87b95d7
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-01-03 20:04:27 +0530

    - refactoring shallow clone example - commiting avro rpc classes

--- core-java-8/src/main/java/com/gauravbytes/clone/shallow/ShallowCloneExample.java
+++ core-java-8/src/main/java/com/gauravbytes/clone/shallow/ShallowCloneExample.java
@@ -12,9 +12,7 @@ public class ShallowCloneExample {
 		Employee employee = new Employee(1, "Gaurav", new Address("Sector 37C", "Chandigarh", "India"));
 		Employee shallowClone = (Employee) employee.clone();
 		employee.getAddress().setCity("Hoshiarpur");
-		// should return false
-		System.out.println(employee.getAddress());
-		System.out.println(shallowClone.getAddress());
+		// should return false but returning true
 		System.out.println(employee.getAddress().equals(shallowClone.getAddress()));
 	}
 }
