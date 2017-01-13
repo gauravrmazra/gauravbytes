d4b984d651cf843d89d2040cadf8ae752f837931
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-01-13 22:54:59 +0530

    Avro Binary read/write example

--- avro-java-class-generation/src/main/java/com/gauravbytes/schemageneration/AvroDatumExample.java
+++ avro-java-class-generation/src/main/java/com/gauravbytes/schemageneration/AvroDatumExample.java
@@ -44,8 +44,32 @@ public class AvroDatumExample {
 		System.out.println(employee);
 	}
 
+	public void binaryReadWriteExample() throws IOException {
+		Employee employee = Employee.newBuilder().setFirstName("Gaurav")
+				.setLastName("Mazra").setSex(SEX.MALE).build();
+
+		DatumWriter<Employee> employeeWriter = new SpecificDatumWriter<>(Employee.class);
+		byte[] data;
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			Encoder jsonEncoder = EncoderFactory.get().binaryEncoder(baos, null);
+			employeeWriter.write(employee, jsonEncoder);
+			jsonEncoder.flush();
+			data = baos.toByteArray();
+		}
+		
+		// serialized data
+		System.out.println(data);
+		
+		DatumReader<Employee> employeeReader = new SpecificDatumReader<>(Employee.class);
+		Decoder decoder = DecoderFactory.get().binaryDecoder(data, null);
+		employee = employeeReader.read(null, decoder);
+		//data after deserialization
+		System.out.println(employee);
+	}
+
 	public static void main(String[] args) throws IOException {
 		AvroDatumExample avroDatumExample = new AvroDatumExample();
 		avroDatumExample.jsonReadWriteExample();
+		avroDatumExample.binaryReadWriteExample();
 	}
 }
