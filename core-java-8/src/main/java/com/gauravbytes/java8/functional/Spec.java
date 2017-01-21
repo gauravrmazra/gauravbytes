369e8b5a8e22681dd855d76dc35e7cf804f0d54c
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-01-21 22:13:22 +0530

    Functional interface example

---
+++ core-java-8/src/main/java/com/gauravbytes/java8/functional/Spec.java
@@ -0,0 +1,26 @@
+package com.gauravbytes.java8.functional;
+
+/**
+ * 
+ * @author Gaurav Rai Mazra
+ *
+ */
+@FunctionalInterface
+public interface Spec<T> {
+	boolean isSatisfiedBy(T t);
+	
+	default Spec<T> not() {
+		return (t) -> !isSatisfiedBy(t);
+	}
+	
+	default Spec<T> and(Spec<T> other) {
+		return (t) -> isSatisfiedBy(t) && other.isSatisfiedBy(t);
+	}
+	
+	default Spec<T> or(Spec<T> other) {
+		return (t) -> isSatisfiedBy(t) || other.isSatisfiedBy(t);
+	}
+	
+	@Override
+	String toString();
+}
