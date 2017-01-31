bf22224b56def0977faa729256cfe8ae5bc0b005
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-01-31 17:09:43 +0530

    reduction example

--- core-java-8/src/main/java/com/gauravbytes/java8/stream/StreamReductionExample.java
+++ core-java-8/src/main/java/com/gauravbytes/java8/stream/StreamReductionExample.java
@@ -1,6 +1,7 @@
 package com.gauravbytes.java8.stream;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import com.gauravbytes.java8.stream.StreamSamples.Person;
 
@@ -13,6 +14,11 @@ public class StreamReductionExample {
 	public static void main(String[] args) {
 		findLongestLine();
 
+		Integer value = StreamSamples.getPersons().stream().map(Person::getAge).reduce(0,
+		    Integer::sum);
+		System.out.println("Sum of persons age: " + value);
+		IntStream.range(1, 10).reduce(Integer::sum)
+		    .ifPresent(sum -> System.out.println("Sum of 1 to 10: " + sum));
 	}
 
 	private static void findLongestLine() {
@@ -20,8 +26,6 @@ public class StreamReductionExample {
 		lines.stream().reduce((a, b) -> a.length() > b.length() ? a : b)
 		    .ifPresent(System.out::println);
 
-		Integer value = StreamSamples.getPersons().stream().map(Person::getAge).reduce(0,
-		    Integer::sum);
-		System.out.println(value);
+
 	}
 }
