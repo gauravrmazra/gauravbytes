8091176a4734a6b906614702865e86fb3cb5a543
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-02-08 23:30:57 +0530

    some comments

--- spring-quickstart/src/main/java/com/gauravbytes/hellogb/App.java
+++ spring-quickstart/src/main/java/com/gauravbytes/hellogb/App.java
@@ -12,6 +12,7 @@ import com.gauravbytes.hellogb.service.GreetingService;
  */
 public class App {
 	public static void main(String[] args) {
+		// using try with resources so that this context closes automatically
 		try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(
 		    "com.gauravbytes.hellogb.service");) {
 			GreetingService greetingService = context.getBean(GreetingService.class);
