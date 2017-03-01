f077a333f349dbbd44642226692237ae1b067c9a
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-03-01 11:52:45 +0530

    removed security module

--- spring-boot-rest-jpa/src/main/java/com/gauravbytes/gkart/App.java
+++ spring-boot-rest-jpa/src/main/java/com/gauravbytes/gkart/App.java
@@ -2,11 +2,7 @@ package com.gauravbytes.gkart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 /**
  * 
@@ -17,18 +13,5 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
-	}
-
-	@Bean
-	BasicAuthenticationFilter basicAuthFilter(AuthenticationManager authenticationManager,
-			BasicAuthenticationEntryPoint basicAuthEntryPoint) {
-		return new BasicAuthenticationFilter(authenticationManager, basicAuthEntryPoint());
-	}
-	
-	@Bean
-	BasicAuthenticationEntryPoint basicAuthEntryPoint() {
-		BasicAuthenticationEntryPoint bauth = new BasicAuthenticationEntryPoint();
-		bauth.setRealmName("GAURAVBYTES");
-		return bauth;
 	}
 }
