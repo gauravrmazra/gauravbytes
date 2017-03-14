4a94dd8210bc502ef2e76c2da382cdc38e08d661
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2017-03-14 14:37:27 +0530

    - packaging corrected - logback log folder corrected

--- spring-basic-auth-example/src/main/java/com/gauravbytes/hellogb/App.java
+++ spring-basic-auth-example/src/main/java/com/gauravbytes/hellogb/App.java
@@ -1,4 +1,4 @@
-package com.gauravbytes.hellgb;
+package com.gauravbytes.hellogb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
