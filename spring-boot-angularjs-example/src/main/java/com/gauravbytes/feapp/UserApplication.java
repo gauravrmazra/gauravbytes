7df56529e41b5fc6d4af51388438b34ca539be43
MazraGR <MazraGR@192.168.1.5>
2017-03-26 18:53:26 +0530

    service side configuration - created controller, service classes and model

---
+++ spring-boot-angularjs-example/src/main/java/com/gauravbytes/feapp/UserApplication.java
@@ -0,0 +1,16 @@
+package com.gauravbytes.feapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Mazra, Gaurav Rais
+ *
+ */
+@SpringBootApplication
+public class UserApplication {
+	public static void main(String[] args) {
+	    SpringApplication.run(UserApplication.class, args);
+	  }
+}
