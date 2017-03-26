c07def939315138a5430ff68b6fdb727f4a11c53
MazraGR <MazraGR@192.168.1.5>
2017-03-26 19:05:51 +0530

    frontend controller, service and index page with angular 1

---
+++ spring-boot-angularjs-example/src/main/resources/static/js/service/user_service.js
@@ -0,0 +1,77 @@
+'use strict';
+
+angular.module('myApp').factory('UserService', ['$http', '$q', function($http, $q){
+
+    var REST_SERVICE_URI = 'http://localhost:8082/users';
+
+    var factory = {
+        fetchAllUsers: fetchAllUsers,
+        createUser: createUser,
+        updateUser:updateUser,
+        deleteUser:deleteUser
+    };
+
+    return factory;
+
+    function fetchAllUsers() {
+        var deferred = $q.defer();
+        $http.get(REST_SERVICE_URI)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while fetching Users');
+                deferred.reject(errResponse);
+            }
+        );
+        return deferred.promise;
+    }
+
+    function createUser(user) {
+        var deferred = $q.defer();
+        $http.post(REST_SERVICE_URI, user)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while creating User');
+                deferred.reject(errResponse);
+            }
+        );
+        return deferred.promise;
+    }
+
+
+    function updateUser(user, id) {
+        var deferred = $q.defer();
+        $http.put(REST_SERVICE_URI + "/" + id, user)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while updating User');
+                deferred.reject(errResponse);
+            }
+        );
+        return deferred.promise;
+    }
+
+    function deleteUser(id) {
+        var deferred = $q.defer();
+        $http.delete(REST_SERVICE_URI + "/" + id)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while deleting User');
+                deferred.reject(errResponse);
+            }
+        );
+        return deferred.promise;
+    }
+
+}]);
