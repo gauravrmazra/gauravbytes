f8176bde05957db516773c14a962adb160a047c8
Gaurav Mazra <gaurav.r.mazra@gmail.com>
2017-05-20 22:05:22 +0530

    Added css in login.html updated security config

--- spring-security-form-based-auth/src/main/java/com/gauravbytes/feapp/config/SecurityConfig.java
+++ spring-security-form-based-auth/src/main/java/com/gauravbytes/feapp/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
         .authorizeRequests()
-            .antMatchers("/static/**", "/", "/index").permitAll()
+            .antMatchers("/static/**", "/", "/index", "/bower_components/**").permitAll()
             .anyRequest().authenticated()
             .and()
         .formLogin()
