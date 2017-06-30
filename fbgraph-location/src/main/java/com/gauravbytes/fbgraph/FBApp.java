300f5db83e5fd5c7951c782763a74a8ec2b5493e
Gaurav Mazra <gaurav.r.mazra@gmail.com>
2017-06-29 21:17:32 -0400

    Initial setup project for Spring Social Facebook example

---
+++ fbgraph-location/src/main/java/com/gauravbytes/fbgraph/FBApp.java
@@ -0,0 +1,16 @@
+package com.gauravbytes.fbgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Mazra, Gaurav Rai
+ *
+ */
+@SpringBootApplication
+public class FBApp {
+	public static void main(String[] args) {
+		SpringApplication.run(FBApp.class, args);
+	}
+}
