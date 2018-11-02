539d61052034b0c8294b26f7351ec65f5315c8ee
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-02 11:30:40 +0530

    EcmaScript 6 TemplateLiteral example

---
+++ es6-lessons/Lesson2 - TemplateLiterals.js
@@ -0,0 +1,10 @@
+let student = {
+    name: 'Gaurav Rai Mazra'
+};
+
+let teacher = {
+    name: 'Rasham Chadha',
+    room: 'N1818'
+}
+
+const message = `${student.name}, please see ${teacher.name} in ${teacher.room} to pick up your report card.`;
