8f858854a3ce6aaf6a20f9238ea9ddae71ac28b7
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-02 15:43:44 +0530

    EcmaScript 6 Lesson 4 Object literals

---
+++ es6-lessons/Lesson 4 - Object Literals.js
@@ -0,0 +1,17 @@
+let name = "Gaurav Mazra";
+let id = 1818;
+let designation = "Senior Software Developer";
+let dateOfBirth = '13/10/1988'
+
+
+var olderWayEmployee = {
+    name: name, 
+    id: id, 
+    designation: designation, 
+    dateOfBirth: dateOfBirth
+};
+
+
+let objectLiteralWayEmployee = { name, id, designation, dateOfBirth, calcuateAge() {
+    
+} };
