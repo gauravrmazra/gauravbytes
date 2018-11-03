6ed33a16b2793f9abe470556c94956f29c1cecf7
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-03 16:39:40 +0530

    explanation added for Array Destructing vs Object destructing with default parameters

--- es6-lessons/Lesson 7 - Default Function Parameters.js
+++ es6-lessons/Lesson 7 - Default Function Parameters.js
@@ -7,3 +7,17 @@ function makeJuice([addSalt = false, addSugar = false, fruits = ['Orange', 'Pome
     const sugar = addSugar ? '': 'no';
     return `Your juice is made from ${fruits.join(' and ')}; has ${salt} salt and ${sugar} sugar added.`;
 }
+
+/** 
+ * Object destructing function is much better choice then array destructing
+ * Because in array destructing if you want to pass only fruits then have to call it like this
+ * makeJuice([undefined, undefined, ['Grapes']]);
+ * Whereas you can call Object Destructing function as follows
+ * makeJuiceWithObjectDestructing({fruits: ['Grapes']});
+ * 
+*/
+function makeJuiceWithObjectDestructing({addSalt = false, addSugar = false, fruits = ['Orange']} = {}) {
+    const salt = addSalt ? '' : 'no';
+    const sugar = addSugar ? '': 'no';
+    return `Your juice is made from ${fruits.join(' and ')}; has ${salt} salt and ${sugar} sugar added.`;
+}
