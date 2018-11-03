512ec9b45014cd47c1a2b7ec8c199fe00777ec54
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-03 16:33:57 +0530

    Default function parameters with array destructing

--- es6-lessons/Lesson 7 - Default Function Parameters.js
+++ es6-lessons/Lesson 7 - Default Function Parameters.js
@@ -1,3 +1,9 @@
 function greet(greeting = "Hello, ", name = "Gaurav") {
     return `${greeting} ${name}`
 }
+
+function makeJuice([addSalt = false, addSugar = false, fruits = ['Orange', 'Pomegranate', 'Pineapple']] = []) {
+    const salt = addSalt ? '' : 'no';
+    const sugar = addSugar ? '': 'no';
+    return `Your juice is made from ${fruits.join(' and ')}; has ${salt} salt and ${sugar} sugar added.`;
+}
