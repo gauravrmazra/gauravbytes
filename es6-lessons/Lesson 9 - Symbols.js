ee519006cba29fa9c53d7f81285aae790bfd481c
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-07 13:35:21 +0530

    EcmaScript 6 Lesson 9 Symbols

--- es6-lessons/Lesson 9 - Symbols.js
+++ es6-lessons/Lesson 9 - Symbols.js
@@ -1,2 +1,16 @@
 // Unique and immutable data-type to identify object's properties
 const symb = Symbol('lion')
+
+const animals = {
+    'lion': { weight: 220.5, length: 155 },
+    'monkey': { weight: 20.1, length: 35 },
+    'lion': { weight: 300, length: 165 }
+}
+
+console.log(animals); // Will print only two objects
+
+const animalWithSymbols = {
+    [Symbol('lion')]: { weight: 220.5, length: 155 },
+    [Symbol('monkey')]: { weight: 20.1, length: 35 },
+    [Symbol('lion')]: { weight: 300, length: 165 }
+}
