ed7a24650b6503b27db04056ff7e1e99e93f825a
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-12-03 21:26:41 +0530

    [WIP]Angular 7 - Removed unnecessary files and changed app prefix.

--- angular7-example/src/app/app.component.ts
+++ angular7-example/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'gb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
