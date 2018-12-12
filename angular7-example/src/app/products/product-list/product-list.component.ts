ebb88c4a9ea4069597708e7420f71498e0d52107
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-12-12 16:46:07 +0530

    [WIP] product listing and routing changes

---
+++ angular7-example/src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gb-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular7-example/src/app/products/products.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  declarations: [ProductListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductsModule { }
