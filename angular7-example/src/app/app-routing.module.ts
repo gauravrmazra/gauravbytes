ce8c71b9f024bba8e37f0fed702de9378c55798a
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-12-19 11:06:52 +0530

    [WIP] Angular 7 examples

--- angular7-example/src/app/app-routing.module.ts
+++ angular7-example/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuardGuard } from './_guards/auth-guard.guard';
+import { ProductsRouting } from './products/products-routing.module';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuardGuard] },
-  { path: '',  pathMatch: 'full', redirectTo: 'login' },
-  {path: 'products', loadChildren: 'app/products/products.module#ProductsModule'}
+  {path: 'products', loadChildren: './products/products.module#ProductsModule', canActivate: [AuthGuardGuard]},
+  { path: '',  pathMatch: 'full', redirectTo: 'login' }
 ];
 
 @NgModule({
--- angular7-example/src/app/products/products.module.ts
+++ angular7-example/src/app/products/products.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductService } from './_services/product.service';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ProductListComponent, ProductsComponent, ProductDetailsComponent],
   imports: [
-    CommonModule,
-    AppRoutingModule
+    RouterModule,
+    CommonModule
   ], providers: [ProductService]
 })
 export class ProductsModule { }
