06086a93fcac09df8d29f2d8ea48fc62df6d9ba6
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2019-01-06 19:35:55 +0530

    It now implements SqlProvider interface as well.

--- springjdbc-examples/src/main/java/com/gauravbytes/springjdbc/ReturnGeneratedKeysPreparedStatementCreator.java
+++ springjdbc-examples/src/main/java/com/gauravbytes/springjdbc/ReturnGeneratedKeysPreparedStatementCreator.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.SqlProvider;
 
 /**
  * This class returns the generated keys by the database
@@ -16,7 +17,7 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
  * @author Gaurav Rai Mazra <a href="https://gauravbytes.com">My blog</a>
  *         <a href="https://lineofcode.in">Website</a>
  */
-public class ReturnGeneratedKeysPreparedStatementCreator implements PreparedStatementCreator {
+public class ReturnGeneratedKeysPreparedStatementCreator implements PreparedStatementCreator, SqlProvider {
 	private final String sql;
 	private String[] generatedColumnNames;
 
@@ -35,6 +36,11 @@ public class ReturnGeneratedKeysPreparedStatementCreator implements PreparedStat
 	public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
 		return generatedColumnNames.length > 0 ? con.prepareStatement(this.sql, this.generatedColumnNames)
 				: con.prepareStatement(this.sql, Statement.RETURN_GENERATED_KEYS);
+	}
+
+	@Override
+	public String getSql() {
+		return this.sql;
 	}
 
 }
