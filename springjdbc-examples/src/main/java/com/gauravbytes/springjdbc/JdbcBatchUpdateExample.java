3851fadd6a46bf558876fc1b2d106be20a55344a
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2019-01-06 17:01:55 +0530

    vertx example version update

--- springjdbc-examples/src/main/java/com/gauravbytes/springjdbc/JdbcBatchUpdateExample.java
+++ springjdbc-examples/src/main/java/com/gauravbytes/springjdbc/JdbcBatchUpdateExample.java
@@ -1,9 +1,13 @@
 package com.gauravbytes.springjdbc;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ParameterizedPreparedStatementSetter;
 
 import com.gauravbytes.springjdbc.ds.JdbcDataSourceUtils;
 import com.gauravbytes.springjdbc.dto.Product;
@@ -25,5 +29,19 @@ public class JdbcBatchUpdateExample {
 						Arrays.asList(new Product("Lenovo Laptop", "laptop", "Thinkpad series laptop"),
 								new Product("Acer Laptop", "laptop", "Predator series laptop"))));
 		log.info(() -> String.format("Inserted rows: %s", Arrays.toString(results)));
+
+		List<Product> products = IntStream.range(0, 10).mapToObj(i -> new Product("A " + i, "mobile", "New mobile"))
+				.collect(Collectors.toList());
+
+		ParameterizedPreparedStatementSetter<Product> pss = (ps, product) -> {
+			ps.setString(1, product.getName());
+			ps.setString(2, product.getCategory());
+			ps.setString(3, product.getDescription());
+		};
+
+		int[][] result = jdbcTemplate.batchUpdate("insert into product (name, category, description) values(?,?,?)",
+				products, 5, pss);
+		
+		log.info(Arrays.deepToString(result));
 	}
 }
