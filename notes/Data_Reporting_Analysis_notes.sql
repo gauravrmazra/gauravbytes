47253d97ae5508ff81240fb2bdd17320f701ba2e
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2019-01-20 23:58:43 +0530

    Notes on SQL for data analytics

--- notes/Data_Reporting_Analysis_notes.sql
+++ notes/Data_Reporting_Analysis_notes.sql
@@ -78,13 +78,73 @@ SELECT first_name, LOCATE('lope', first_name) as located_at
 FROM `actor` where LOCATE('lope', first_name) > 0;
 
 
+-- Aggregating functions
 
 
+-- GROUP BY
+SELECT district, count(*) as addresses_per_district
+FROM `address`
+GROUP BY district
+ORDER BY addresses_per_district;
+
+-- HAVING Clause
+SELECT district, count(*) as addresses_per_district
+FROM `address`
+GROUP BY district
+HAVING addresses_per_district > 8
+ORDER BY addresses_per_district;
 
 
+-- MAX function
+SELECT MAX(rental_duration) max_rental_duration FROM `film`;
+
+-- MIN function
+SELECT MIN(rental_duration) min_rental_duration FROM `film`;
+
+-- SUM function
+SELECT SUM(rental_duration) sum_of_rental_duration from `film`;
+
+-- AVG function
+SELECT rating, avg(rental_duration) as avg_rental_duration
+FROM `film`
+GROUP BY rating;
 
 
+-- distinct clause
+SELECT DISTINCT district, group_concat(phone) AS customer_phones
+FROM address
+GROUP BY district;
 
+SELECT DISTINCT district, group_concat(phone ORDER BY phone ASC SEPARATOR ';') AS customer_phones
+FROM address
+GROUP BY district;
 
+-- Joining tables
+SELECT c.first_name, c.last_name, CONCAT(a.address, ' ', a.address2, ' ', a.district) AS ADDRESS 
+FROM `customer` AS c 
+JOIN `address` AS a
+ON c.address_id = a.address_id
+WHERE a.district = 'Buenos Aires';
 
+SELECT c.first_name, c.last_name, CONCAT(a.address, ' ', a.address2,' ', a.district, ' ', cty.city, ' ', ctry.country) AS ADDRESS 
+FROM `customer` AS c 
+JOIN `address` AS a
+ON c.address_id = a.address_id
+JOIN `city` AS cty
+ON a.city_id = cty.city_id
+JOIN `country` AS ctry
+ON cty.country_id = ctry.country_id
+WHERE a.district = 'Buenos Aires';
 
+-- UNION example
+SELECT 'actor' as tbl, date(last_update) last_updated FROM `actor`
+UNION ALL
+SELECT 'address' as tbl, date(last_update) last_updated FROM `address`;
+
+-- IN QUERY on multiple tables
+SELECT * FROM `rental`
+WHERE customer_id IN (
+	SELECT customer_id 
+    FROM `customer` 
+    WHERE first_name = 'Jennifer'
+);
