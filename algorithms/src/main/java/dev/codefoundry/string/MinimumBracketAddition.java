944c4587271ff2384859a4c322e202424ede3866
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2020-09-28 21:01:12 -0400

    String algorithms

---
+++ algorithms/src/main/java/dev/codefoundry/string/MinimumBracketAddition.java
@@ -0,0 +1,46 @@
+package dev.codefoundry.string;
+
+/**
+ * 
+ * @author Gaurav Rai Mazra
+ * {@link https://codefoundry.dev}
+ * 
+ * Given a string of Parenthesis, return how many are not closed 
+ *
+ */
+public class MinimumBracketAddition {
+	public int solve(String s) {
+        int requiredBrackets = 0;
+        char lastBracket = ' ';
+        int count = 0;
+        
+        // ))(()))((
+        for (int i = 0; i < s.length(); i++) {
+        	if(lastBracket == ' ') {
+        		lastBracket = s.charAt(i);
+        		requiredBrackets += count;
+        		count = 1;
+        	} else if (lastBracket == s.charAt(i)) {
+        		count++;
+        	} else if (lastBracket == '(' && s.charAt(i) == ')') {
+        		count--;
+        		
+        		if (count == 0) {
+        			lastBracket = ' ';
+        		}
+        	} else if (lastBracket == ')' && s.charAt(i) == '(') {
+        		requiredBrackets += count;
+        		lastBracket = '(';
+        		count = 1;
+        	}
+        }
+        
+        requiredBrackets += count;
+        return requiredBrackets;
+    }
+	
+	public static void main(String[] args) {
+		MinimumBracketAddition mba = new MinimumBracketAddition();
+		System.out.println(mba.solve(")))((") == 5);
+	}
+}
