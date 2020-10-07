1d26e70e59e814481f68bee854755e296e3bcb8e
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2020-10-07 18:32:46 -0400

    String algorithm

--- algorithms/src/main/java/dev/codefoundry/string/ShortestString.java
+++ algorithms/src/main/java/dev/codefoundry/string/ShortestString.java
@@ -2,19 +2,35 @@ package dev.codefoundry.string;
 
 /**
  * 
- * @author Gaurav Rai Mazra
- * {@link https://www.codefoundry.dev}
- * Consider a string consist of 1 and 0. You can delete character
- * if the consective chars are not same
- * Return the shortsest string length
+ * @author Gaurav Rai Mazra {@link https://www.codefoundry.dev} Consider a
+ *         string consist of 1 and 0. You can delete character if the consective
+ *         chars are not same Return the shortsest string length
  */
 public class ShortestString {
 	public int solve(String s) {
-		return s.length();
-	}
-	
+        // Write your code here
+        int i = 0;
+        int j = 1;
+        int len = s.length();
+        if (len == 1) {
+            return len;
+        }
+        String str = s;
+        while (i < str.length() && j < str.length()) {
+            if (str.charAt(i) != str.charAt(j)) {
+                str = str.substring(0, i) + str.substring(j + 1);
+                i = 0;
+                j = 1;
+            } else {
+            	i++;
+                j++;	
+            }
+        }
+    
+        return str.length();
+    }
+
 	public static void main(String[] args) {
-		//System.out.println(new ShortestString().solve("11000"));
-		System.out.println('b' - 97);
+		System.out.println(new ShortestString().solve("11000"));
 	}
 }
