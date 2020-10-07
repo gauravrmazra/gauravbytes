5cedaeb30f71620df9314927de40f6e146be7ee8
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2020-10-07 18:32:24 -0400

    Few algo on List and Stack

---
+++ algorithms/src/main/java/dev/codefoundry/list/ListPartitioning.java
@@ -0,0 +1,30 @@
+package dev.codefoundry.list;
+
+import java.util.Arrays;
+
+public class ListPartitioning {
+	public String[] solve(String[] strs) {
+		Arrays.parallelSort(strs, (String first, String second) -> {
+			return rank(first) - rank(second);
+		});
+		
+		return strs;
+	}
+
+	public int rank(String str) {
+		switch (str) {
+		case "red":
+			return 0;
+		case "green":
+			return 1;
+		case "blue":
+			return 2;
+		default:
+			return -1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Arrays.deepToString(new ListPartitioning().solve(new String[] {"green", "blue", "red", "red"})));
+	}
+}
---
+++ algorithms/src/main/java/dev/codefoundry/stack/UnixPathResolution.java
@@ -0,0 +1,32 @@
+package dev.codefoundry.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 
+ * @author Gaurav Rai Mazra
+ * {@link https://www.codefoundry.dev}
+ *
+ * For list of path, if you encounter .. then move to previous directory and if you encounter . keep in same directory
+ * return final array
+ */
+public class UnixPathResolution {
+	public String[] solve(String[] path) {
+		Deque<String> stack = new LinkedList<String>();
+		for (String p : path) {
+			if (p.equals("..")) {
+				String lastPath = stack.peekLast();
+				if (lastPath != null) {
+					stack.removeLast();	
+				}
+			} else if (p.equals(".")) {
+				stack.peekLast();
+			} else {
+				stack.addLast(p);
+			}
+		}
+		
+		return stack.toArray(new String[stack.size()]);
+	}
+}
