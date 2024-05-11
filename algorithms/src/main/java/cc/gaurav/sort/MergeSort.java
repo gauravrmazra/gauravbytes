1e16b62514054bf145df4bf1b5b137ce9d05dd19
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2024-05-11 14:12:01 -0400

    Update mergesort algo

--- algorithms/src/main/java/cc/gaurav/sort/MergeSort.java
+++ algorithms/src/main/java/cc/gaurav/sort/MergeSort.java
@@ -4,55 +4,55 @@ import java.util.Arrays;
 
 public class MergeSort {
 	public void sort(int[] unsorted) {
-		mergeSort(unsorted, 0, unsorted.length);
+		divide(unsorted, 0, unsorted.length - 1);
 	}
 	
-	private void mergeSort(int[] elements, int start, int end) {
-		if (start < end) {
-			int mid = start + (end - start) / 2;
-			mergeSort(elements, start, mid);
-			mergeSort(elements, mid + 1, end);
-			
-			merge(elements, start, mid, end);
+	private static void divide(int[] arr, int lo, int hi) {
+		if (lo < hi) {
+		  int mid = (lo + hi) / 2;
+		  divide(arr, lo, mid);
+		  divide(arr, mid + 1, hi);
+		  merge(arr, lo, mid, hi);
 		}
-	}
-	
-	private void merge(int[] elements, int start, int mid, int end) {
-		
-		int[] left = new int[ mid + 1 - start];
-		int[] right = new int[end - mid];
-		
-		for (int i = 0; i < left.length; i++) {
-		  left[i] = elements[start + i];	
+	  }
+  
+	  private static void merge(int[] arr, int lo, int mid, int hi) {
+		int leftSize = mid - lo + 1;
+		int rightSize = hi - mid;
+		int[] left = new int[leftSize];
+		int[] right = new int[rightSize];
+  
+		for(int i = 0; i < leftSize; ++i) {
+		  left[i] = arr[i + lo];
 		}
-		
-		for (int i = 0; i < right.length; i++) {
-			right[i] = elements[mid + i];
+  
+		for (int i = 0; i < rightSize; ++i) {
+		  right[i] = arr[mid + 1 + i];
 		}
-		
-		int m = 0;
-		int n = 0;
-		
-		int k = start;
-		while(m < left.length && n < right.length) {
-			if (left[m] <= right[n]) {
-				elements[k++] = left[m++];
-			} else {
-				elements[k++] = right[n++];
-			}
+  
+		int i = 0;
+		int j = 0;
+		int k = lo;
+  
+		while(i < leftSize && j < rightSize) {
+		  if (left[i] <= right[j]) {
+			arr[k++] = left[i++];
+		  } else {
+			arr[k++] = right[j++];
+		  }
 		}
-		
-		while(m < left.length) {
-			elements[k++] = left[m++];
+  
+		while(i < leftSize) {
+		  arr[k++] = left[i++];
 		}
-		
-		while(n < right.length) {
-			elements[k++] = right[n++];
+  
+		while(j < rightSize) {
+		  arr[k++] = right[j++];
 		}
-	}
+	  }
 	
 	public static void main(String[] args) {
-		int[] nums = new int[] {1, 10, 2, 20, 1, 5, 6, 87, 12};
+		int[] nums = new int[] {1, 10, 2, 20, 1, 5, 6, 87,22, 3};
 		new MergeSort().sort(nums);
 		System.out.println(Arrays.toString(nums));
 	}
